66e7bae031f0e4cf64d5e62a7caeb614cf86d8de
Christopher Hunt <huntchr@gmail.com>
2014-11-12 12:16:50 +1100

    Corrected declaration mistake

--- project/Build.scala
+++ project/Build.scala
@@ -38,7 +38,7 @@ object Build extends AutoPlugin {
             "typesafe-releases" -> url(s"$typesafe/maven-releases/")
         Some(sbt.Resolver.url(name, u))
       },
-      release-cross-build := true,
+      ReleaseKeys.crossBuild := true,
       // Scalariform settings
       ScalariformKeys.preferences := ScalariformKeys.preferences.value
         .setPreference(AlignSingleLineCaseStatements, true)
